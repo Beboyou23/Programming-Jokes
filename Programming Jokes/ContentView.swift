6323ec76f5af4e0a8950fa66a273c33ab8a0896b
Jordan Rarrat <Jrarrat@bsd220.org>
2021-02-21 19:51:35 -0600

    Got Api key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -26,13 +26,7 @@ struct ContentView: View {
         })
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
-    }
+        let apiKey = "?rapidapi-key=f85e60ae43mshae649811fe5b185p147232jsn0490791bc83a"
 }
 
 struct ContentView_Previews: PreviewProvider {
